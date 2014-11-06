34edb26c154bf022df81d4309d39ca3bc94a88c7
root <csanders@sparsa.org>
2014-11-06 10:34:48 -0500

    Added RC4

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 #include <locale.h>
 #include <sysexits.h>
 #include <stdlib.h>
-#include <curl/curl.h>
+#include <curl/curl.h> // For web request
 #include <unistd.h>
 #include <errno.h>
 #include <sys/types.h>
@@ -36,6 +36,99 @@ void get_mac_address(int socketHandle){
 
 }
 #endif
+
+static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                                'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                                '4', '5', '6', '7', '8', '9', '+', '/'};
+static char *decoding_table = NULL;
+static int mod_table[] = {0, 2, 1};
+
+void build_decoding_table() {
+    int i;
+    decoding_table = malloc(256);
+
+    for (i = 0; i < 64; i++)
+        decoding_table[(unsigned char) encoding_table[i]] = i;
+}
+
+char *base64_encode(const unsigned char *data,
+                    size_t input_length,
+                    size_t *output_length) {
+    int i,j;
+    *output_length = ((input_length - 1) / 3) * 4 + 4;
+
+    char *encoded_data = malloc(*output_length);
+    if (encoded_data == NULL) return NULL;
+
+    for (i = 0, j = 0; i < input_length;) {
+
+        uint32_t octet_a = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_b = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_c = i < input_length ? (unsigned char)data[i++] : 0;
+
+        uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+
+        encoded_data[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 1 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
+    }
+
+    for (i = 0; i < mod_table[input_length % 3]; i++)
+        encoded_data[*output_length - 1 - i] = '=';
+
+    return encoded_data;
+}
+
+
+unsigned char *base64_decode(const char *data,
+                             size_t input_length,
+                             size_t *output_length) {
+    int i,j;
+    if (decoding_table == NULL) build_decoding_table();
+
+    if (input_length % 4 != 0) return NULL;
+
+    *output_length = input_length / 4 * 3;
+    if (data[input_length - 1] == '=') (*output_length)--;
+    if (data[input_length - 2] == '=') (*output_length)--;
+
+    unsigned char *decoded_data = malloc(*output_length);
+    if (decoded_data == NULL) return NULL;
+
+    for (i = 0, j = 0; i < input_length;) {
+
+        uint32_t sextet_a = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_b = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_c = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_d = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+
+        uint32_t triple = (sextet_a << 3 * 6)
+        + (sextet_b << 2 * 6)
+        + (sextet_c << 1 * 6)
+        + (sextet_d << 0 * 6);
+
+        if (j < *output_length) decoded_data[j++] = (triple >> 2 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 1 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 0 * 8) & 0xFF;
+    }
+
+    return decoded_data;
+}
+
+
+
+
+
+void base64_cleanup() {
+    free(decoding_table);
+}
+
 char* getPrimaryIP(){
     char *addressBuffer = malloc(INET_ADDRSTRLEN);
     int socketHandle;
@@ -85,8 +178,65 @@ char* getPrimaryIP(){
 
     
 }
+char *rc4(char *str, const char *key){
+    char * a;
+    unsigned char s[256];
+    unsigned char r[256];
+    int i,j,x,y,t;
+    for(i = 0;i<256;i++){
+        r[i] = key[i % (int)strlen(key)];
+        s[i] = i;
+    }
+    j = 0;
+    for(i=0;i<256;i++){
+        j = (j + s[i] + r[i]) % 256;
+        x = s[i];
+        s[i] = s[j];
+        s[j] = x;
+    }
+    i = 0;
+    j = 0;
+    a = (char*) malloc(strlen(str));
+    if (a==NULL) exit (1);
+    for(y=0;y<strlen(str);y++){
+        i = (i+1) % 256;
+        j = (j + s[i]) % 256;
+        x = s[i];
+        s[i] = s[j];
+        s[j] = x;
 
+        t = (s[(s[i] + s[j]) % 256]);
+        if(t == str[y]){
+            a[y] = (str[y]);
+        }else{
+            a[y] = (t ^ str[y]);
+        }
+        
+    }
+
+    return a;
+}
 int main(int argc, char *argv[]){
+    const char *key = "ZAQwsxcde321";
+    char *t = rc4("test",key);
+    printf("%s\n",rc4(t,key));
+    size_t x;
+    printf("%s\n",base64_encode(t,strlen(t),&x));
+
+    CURL *curl;
+    CURLcode res;
+    curl = curl_easy_init();
+    if(curl){
+           curl_easy_setopt(curl,CURLOPT_URL,"http://172.16.1.15/ThrowbackLP/");
+           curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
+    }
+    res = curl_easy_perform(curl);
+    if(res != CURLE_OK){
+        fprintf(stderr, "curl_easy_perform() failed %s\n",curl_easy_strerror(res));
+    }
+    curl_easy_cleanup(curl);
+    
+
     char hostname[HOST_NAME_MAX];
     struct ifaddrs * ifAddrStruct=NULL;
     struct ifaddrs * ifa=NULL;
